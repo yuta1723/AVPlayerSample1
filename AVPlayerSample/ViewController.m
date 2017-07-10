e2f8409d15352eda4c42a8d8dd3acc949162b000
yuta naito <y.naito@suvt.co.jp>
2017-07-10 14:44:16 +0900

    control center のボタンをactiveに変更

--- AVPlayerSample/ViewController.m
+++ AVPlayerSample/ViewController.m
@@ -8,6 +8,8 @@
 
 #import "ViewController.h"
 #import "AVPlayerView.h"
+#import <MediaPlayer/MediaPlayer.h>
+
 
 @interface ViewController ()
 
@@ -40,6 +42,7 @@
     [nc addObserver:self selector:@selector(viewDidEnterBackground) name:@"applicationDidEnterBackground" object:nil];
     [nc addObserver:self selector:@selector(viewWillEnterForeground) name:@"applicationWillEnterForeground" object:nil];
     
+    
     [self createPlayerInstance];
 }
 
@@ -65,11 +68,11 @@
     
     NSError *setCategoryError = nil;
     BOOL success = [_audioSession setCategory:AVAudioSessionCategoryPlayback error:&setCategoryError];
-    //    BOOL success = [audioSession setCategory:AVAudioSessionCategoryAmbient error:&setCategoryError];
+//        BOOL success = [_audioSession setCategory:AVAudioSessionCategoryAmbient error:&setCategoryError];
     if (!success) {
         NSLog(@"naito : can not create audioSession");
         /* handle the error condition */
-        return NO;
+//        return NO;
     }
     
     NSError *activationError = nil;
@@ -86,6 +89,7 @@
 {
     [self createAudioSessionInstance];
     NSURL *url = [NSURL URLWithString:@"http://domain/path/contents.mp4"];
+    [self attachRemoteCommandCenter];
     _player = [[AVPlayer alloc]initWithURL:url];
     _playerView = [[AVPlayerView alloc]initWithFrame:CGRectMake(0,20,self.view.frame.size.width,300)];
     [(AVPlayerLayer*)_playerView.layer setPlayer:_player];
@@ -94,13 +98,77 @@
     
     [self play];
     [self setUpRemoteControllers];
-
     
 }
+- (void) attachRemoteCommandCenter {
+    //addTargetを行うことで有効化された
+    MPRemoteCommandCenter *commandCenter = [MPRemoteCommandCenter sharedCommandCenter];
+    [commandCenter.pauseCommand setEnabled:YES];
+    [commandCenter.pauseCommand addTarget:self action:@selector(onPushedplayCommand)];
+//    [commandCenter.togglePlayPauseCommand setEnabled:YES];
+//    [commandCenter.togglePlayPauseCommand addTarget:self action:@selector(onPushedtoggleCommand)];
+    [commandCenter.skipBackwardCommand setEnabled:YES];
+    [commandCenter.skipBackwardCommand addTarget:self action:@selector(onSkipBackwardCommand)];
+    [commandCenter.skipForwardCommand setEnabled:YES];
+    [commandCenter.skipForwardCommand addTarget:self action:@selector(onSkipForwardCommand)];
+    
+    [commandCenter.seekForwardCommand setEnabled:YES];
+    [commandCenter.seekBackwardCommand setEnabled:YES];
+}
+
+-(BOOL) onPushedPreCommand
+{
+    NSLog(@"naito : onPushedPreCommand");
+    [self playOrPause];
+    
+    return YES;
+}
+
+-(BOOL) onSkipBackwardCommand
+{
+    NSLog(@"naito : onSkipBackwardCommand");
+    [self playOrPause];
+    
+    return YES;
+}
+
+-(BOOL) onSkipForwardCommand
+{
+    NSLog(@"naito : onSkipForwardCommand");
+    [self playOrPause];
+    
+    return YES;
+}
+
+-(BOOL) onPushedForwardCommand
+{
+    NSLog(@"naito : onPushedForwardCommand");
+    [self playOrPause];
+    
+    return YES;
+}
+
+-(BOOL) onPushedplayCommand
+{
+    NSLog(@"naito : onPushedplayCommand");
+    [self pause];
+    
+    return YES;
+}
+
+-(BOOL) onPushedtoggleCommand
+{
+    NSLog(@"naito : pause");
+    [self playOrPause];
+    
+    return YES;
+}
+
 -(BOOL) pause
 {
     NSLog(@"naito : pause");
     [_player pause];
+    
     return YES;
 }
 -(BOOL) play
@@ -111,38 +179,50 @@
     return YES;
 }
 
-- (void)remoteControlReceivedWithEvent:(UIEvent *)receivedEvent
-{
-    if (receivedEvent.type == UIEventTypeRemoteControl) {
-        
-        switch (receivedEvent.subtype) {
-                
-            case UIEventSubtypeRemoteControlPlay:
-                NSLog(@"pressed : UIEventSubtypeRemoteControlPlay");
-                [self updateRemoteControllers];
-                [self pause];
-                break;
-            case UIEventSubtypeRemoteControlPause:
-                NSLog(@"pressed : UIEventSubtypeRemoteControlPause");
-                [self updateRemoteControllers];
-                [self play];
-                break;
-            case UIEventSubtypeRemoteControlTogglePlayPause:
-                NSLog(@"pressed : UIEventSubtypeRemoteControlTogglePlayPause");
-                [self updateRemoteControllers];
-                [self playOrPause];
-                break;
-            case UIEventSubtypeRemoteControlNextTrack:
-                NSLog(@"pressed : UIEventSubtypeRemoteControlNextTrack");
-                break;
-            case UIEventSubtypeRemoteControlPreviousTrack:
-                NSLog(@"pressed : UIEventSubtypeRemoteControlPreviousTrack");
-                break;
-            default:
-                break;
-        }
-    }
-}
+//- (void)remoteControlReceivedWithEvent:(UIEvent *)receivedEvent
+//{
+//    if (receivedEvent.type == UIEventTypeRemoteControl) {
+//
+//        switch (receivedEvent.subtype) {
+//
+//            case UIEventSubtypeRemoteControlPlay:
+//                NSLog(@"pressed : UIEventSubtypeRemoteControlPlay");
+////                [self updateRemoteControllers];
+//                [self pause];
+//                break;
+//            case UIEventSubtypeRemoteControlPause:
+//                NSLog(@"pressed : UIEventSubtypeRemoteControlPause");
+////                [self updateRemoteControllers];
+//                [self play];
+//                break;
+////            case UIEventSubtypeRemoteControlTogglePlayPause:
+////                NSLog(@"pressed : UIEventSubtypeRemoteControlTogglePlayPause");
+//////                [self updateRemoteControllers];
+////                [self playOrPause];
+////                break;
+//            case UIEventSubtypeRemoteControlNextTrack:
+//                NSLog(@"pressed : UIEventSubtypeRemoteControlNextTrack");
+//                break;
+//            case UIEventSubtypeRemoteControlPreviousTrack:
+//                NSLog(@"pressed : UIEventSubtypeRemoteControlPreviousTrack");
+//                break;
+//            case UIEventSubtypeRemoteControlBeginSeekingBackward:
+//                NSLog(@"pressed : UIEventSubtypeRemoteControlBeginSeekingBackward");
+//                break;
+//            case UIEventSubtypeRemoteControlBeginSeekingForward:
+//                NSLog(@"pressed : UIEventSubtypeRemoteControlBeginSeekingForward");
+//                break;
+//            case UIEventSubtypeRemoteControlEndSeekingBackward:
+//                NSLog(@"pressed : UIEventSubtypeRemoteControlEndSeekingBackward");
+//                break;
+//            case UIEventSubtypeRemoteControlEndSeekingForward:
+//                NSLog(@"pressed : UIEventSubtypeRemoteControlEndSeekingForward");
+//                break;
+//            default:
+//                break;
+//        }
+//    }
+//}
 
 - (void)playOrPause
 {
@@ -167,6 +247,7 @@
                                   };
 //    [playingInfo setObject:[NSNumber numberWithFloat:0] forKey:MPNowPlayingInfoPropertyPlaybackRate];
     [center setNowPlayingInfo:contentInfo];
+    
 }
 
 -(void) updateRemoteControllers
